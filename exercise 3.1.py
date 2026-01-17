6ef5150e9b11fa22742f18e026027605d786051d
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-01-17 13:01:33 +0200

    3.1 successfull

---
+++ exercise 3.1.py
@@ -0,0 +1,10 @@
+#Write a program that asks a fisher the length of a zander in centimeters. If the zander does not fulfill the size limit, the program instructs to release the fish back into the lake and notifies the user of how many centimeters below the size limit the caught fish was. A zander must be 42 centimeters or longer to meet the size limit.
+
+zander = float(input("enter the lenght of the zander in centimeters:"))
+
+if zander < 42:
+    print("release the fish back into the lake")
+if zander < 42:
+    print("your fis is:", (42-zander) , "below the limit")
+else:
+    print ("you can keep the fish :)")
